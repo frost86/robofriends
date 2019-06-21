822295fcaaf6eb64dd30dc856e99e73ab0a883c0
Michael Lay <michaeldlay@michaels-imac.lan>
2019-06-21 14:00:36 -0400

    connecting React with Redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,30 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
+
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
 
 class App extends Component {
     constructor(){
         super()
         this.state = {
             //These change in the app and will render()
-            robots: [],
-            searchfield: ''
+            robots: []
         }
     }
 
@@ -20,19 +34,15 @@ class App extends Component {
         .then(users=>this.setState({robots: users}));
     }
 
-    onSearchChange = (event) => {
-        //To update state of searchfield
-        this.setState({searchfield: event.target.value})
-    }
-
     render(){
-        const{robots, searchfield} = this.state;
+        const{robots} = this.state;
+        const {searchField, onSearchChange} = this.props;
         const filteredRobots = robots.filter(robot => {
             /*This will return the robots (in lower case) if it matches
             the value in the search box (in lower case). The lower case
             function is used here because it's easier to compare what's
             written.*/
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
         /*What if there are a lot of users and it's slow? Add IF statement to see 
         if there are robots.*/
@@ -40,7 +50,7 @@ class App extends Component {
         (
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <ErrorBoundary>
                         <CardList robots ={filteredRobots}/>
@@ -51,4 +61,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
