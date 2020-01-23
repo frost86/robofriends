e0e2998a2277ea7731fb8d8b97c775094dd0cca8
Michael Lay <michaeldlay@Michaels-iMac.local>
2020-01-23 10:47:59 -0500

    prep for GitHub Pages

---
+++ build/precache-manifest.45f65a73065ef5390e16acbee4195e5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "974fcf2de85248863f4de628591b46b8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a65429955a8427e6b1c6",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3f3d48348e20ac9297b7",
+    "url": "/robofriends/static/css/main.487c52d3.chunk.css"
+  },
+  {
+    "revision": "a65429955a8427e6b1c6",
+    "url": "/robofriends/static/js/2.ed288492.chunk.js"
+  },
+  {
+    "revision": "4cdd2411cb4fa1c1c4ef19dc4dc1d081",
+    "url": "/robofriends/static/js/2.ed288492.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f3d48348e20ac9297b7",
+    "url": "/robofriends/static/js/main.e35ed497.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
